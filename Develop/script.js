5d57cc5a91b0b5f22baab37b6a7c77cbe0a4dd9b
nguyen-william93 <=>
2021-08-27 00:19:28 -0500

    started to declaring golbal variable in script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,19 @@
 // Assignment code here
 
+//global variable declaration and object to store user choice
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var minLength = 8; //8 because it's the bare minumun
+userChoice = {
+
+};
+
+//randomNumber function to create a random number betweeb 0 and 25 (0 index) -- math.random * (25-0+1)
+var randomNumberAlphabet = function (){
+  var value = Math.floor(Math.random()* (26));
+  return value;
+};
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
